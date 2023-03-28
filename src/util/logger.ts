4286e40bc71cae277f7b2635c195720daffe0a82
Lucas Larangeira <lukearch@beblitz.com.br>
2023-03-28 16:08:25 +0000

    update

---
+++ src/util/logger.ts
@@ -0,0 +1,18 @@
+import { Formats, jetLogger, LoggerModes } from 'jet-logger'
+
+const logger = jetLogger(LoggerModes.Console, '', false, false, Formats.Line)
+
+export default {
+  info: (message: string): void => {
+    logger.info(message)
+  },
+  warn: (message: string): void => {
+    logger.warn(message)
+  },
+  err: (message: string): void => {
+    logger.err(message)
+  },
+  imp: (message: string): void => {
+    logger.imp(message)
+  },
+} as const
